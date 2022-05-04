947a0350db749b49c1f2a3bbf3a841296d2bcbd2
loichu <loic@humbert-web.dev>
2022-05-04 12:06:29 +0200

    clean

--- API-old/E2E/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Subject, Topic, User
-from django.contrib.auth.admin import UserAdmin
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Subject)
-admin.site.register(Topic)
--- API-old/E2E/api/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .chat import Chat, Message
-from .meeting import Grade, Meeting
-from .users import User, Student
-from .topics import Subject, Topic
